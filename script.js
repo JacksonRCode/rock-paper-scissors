2fd1a27945905a2ffc802ef9646bd438e87b5020
JacksonRCode <jackdareid@gmail.com>
2023-05-18 23:12:46 -0700

    Fix script bugs and add new readability functions

--- script.js
+++ script.js
@@ -24,21 +24,89 @@ function makeCheckChoice() {
             console.log("Incorrect entry1, please enter again\n");
             continue;
         }
+
         let upperChoice = uncheckedChoice.toUpperCase();
-        console.log(upperChoice);
+
         if (upperChoice !== "ROCK" && upperChoice !== "PAPER" && upperChoice !== "SCISSORS" ) {
             console.log("Incorrect entry, please enter again\n")
             continue;
         }
-    
         choice = true;
     }
 
     return uncheckedChoice;
-
 }
 
-let compChoice = getComputerChoice();
-let playerChoice = makeCheckChoice().toUpperCase();
+function playRound(user, comp) {
+    let tie=true;
+    while(tie==true) {
+        // Ties
+        if (user==="ROCK" && comp==="ROCK") {
+            alert("TIE!")
+            return "TIE";
+        }
+        else if (user==="PAPER" && comp==="PAPER") {
+            alert("TIE!")
+            return "TIE";
+        }
+        else if (user==="SCISSORS" && comp==="SCISSORS") {
+            alert("TIE!")
+            return "TIE";
+        }
+        // Comp wins
+        else if (user==="ROCK" && comp==="PAPER") {
+            tie=false;
+            alert("COMPUTER WINS THIS ROUND!")
+            return false;
+        }
+        else if (user==="PAPER" && comp==="SCISSORS") {
+            tie=false;
+            alert("COMPUTER WINS THIS ROUND!")
+            return false;
+        }
+        else if (user==="SCISSORS" && comp==="ROCK") {
+            tie=false;
+            alert("COMPUTER WINS THIS ROUND!")
+            return false;        
+        }
+        tie=false;
+        alert("USER WINS THIS ROUND!")
+        return true;
+    }
+}
 
-console.log(playerChoice, compChoice);
+function victory(winner, winnerCount, loserCount) {
+    console.log(winner + " WINS!!!\n\nTHE SCORE WAS: \n" + winnerCount + " FOR THE WINNER, AND\n" + loserCount + " FOR THE LOSER");
+
+    return;
+}
+
+let compCount=0;
+let userCount=0;
+
+while(compCount != 5 && userCount != 5) {
+
+    let compChoice = getComputerChoice();
+    let playerChoice = makeCheckChoice().toUpperCase();
+    let result = playRound(playerChoice, compChoice)
+    
+    if (result == "TIE") {
+        continue;
+    }
+    else if (result) {
+        userCount++;
+    }
+    else if (!result) {
+        compCount++;
+    }
+    
+    if (userCount === 5) {
+        victory("USER", userCount, compCount);
+        break;
+    }
+    else if (compCount === 5) {
+        victory("COMPUTER", compCount, userCount);
+        break;
+    }
+
+}
